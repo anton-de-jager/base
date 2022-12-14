49baa67eccf601604cb8205ab8b40031966330c6
Anton de Jager <adejager4@oldmutual.com>
2022-12-14 14:48:17 +0200

    first Build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.loadgistix.www',
+  appName: 'loadgistix',
+  webDir: 'dist/fuse',
+  bundledWebRuntime: false
+};
+
+export default config;
